2df46b9f4335cec9eba78d6b2582fea840b68bc6
hamedbinazeez90 <hamedbinazeez90@gmail.com>
2026-05-17 15:36:42 +0530

    Update abortnetworkcall.spec.js
    abort done

--- tests/abortnetworkcall.spec.js
+++ tests/abortnetworkcall.spec.js
@@ -7,7 +7,7 @@ test.only('End to End product order',async ({browser})=>
  
 const context =await browser.newContext();
 const page =await context.newPage();
-page.route('**/*.{jpg,png,jpeg}',route=>route.abort())
+page.route('**/*.{jpg,png,jpeg}',route=>route.abort())//abort network calls
  
 // await page.goto("https://rahulshettyacademy.com/client/#/auth/register/");await page.locator(".text-reset").click();
 await page.goto("https://rahulshettyacademy.com/client/");
